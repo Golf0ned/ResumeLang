7e5e0eead6cc0086ac949aa912d222ef760ca3ef
Golf0ned <benjaminye.email@gmail.com>
2025-04-06 00:57:23 -0500

    build out compiler + template replacement

---
+++ compiler/compiler.py
@@ -0,0 +1,64 @@
+import os
+import json
+
+import jinja2
+
+from grammar import resume
+
+
+class ResumeLang:
+    def __init__(self, file):
+        self.file = file
+        self.data = self.parse()
+
+    def parse(self):
+        if not self.file.endswith(".resume"):
+            raise ValueError("File must have a .resume extension")
+
+        with open(self.file, "r", encoding="utf-8") as f:
+            data = f.read()
+            result = resume.parseString(data)
+            return result.as_dict()
+
+    def emit_json(self, file=None):
+        res = json.dumps(self.data, indent=4)
+
+        if not file:
+            print(res)
+            return
+
+        with open(file, "w", encoding="utf-8") as f:
+            f.write(res)
+
+    def write_template(self, template, output=None):
+        env = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates/"),
+            block_start_string="<<",
+            block_end_string=">>",
+            variable_start_string="<;",
+            variable_end_string=";>",
+            comment_start_string="<#",
+            comment_end_string="#>",
+            trim_blocks=True,
+        )
+
+        unfilled = env.get_template(template)
+        res = unfilled.render(self.data)
+
+        if not output:
+            print(res)
+            return
+        with open(output, "w", encoding="utf-8") as f:
+            f.write(res)
+
+if __name__ == "__main__":
+    import sys
+    
+    if len(sys.argv) != 2:
+        print("Usage: python compiler.py <file.resume>")
+        sys.exit(1)
+
+    file = sys.argv[1]
+    resume_lang = ResumeLang(file)
+    # resume_lang.emit_json()
+    resume_lang.write_template("latex/jakes_resume.tex")
--- compiler/grammar.py
+++ compiler/grammar.py
@@ -8,10 +8,34 @@ rp = pp.Suppress(")")
 
 quoted_string = pp.QuotedString('"')
 
-date_half = pp.Word(pp.nums)
-date_format = date_half + pp.Suppress("-") + date_half
+# replace number with abbreviated month
+date_month = (
+    pp.Literal("1").setParseAction(pp.replaceWith("Jan")) |
+    pp.Literal("01").setParseAction(pp.replaceWith("Jan")) |
+    pp.Literal("2").setParseAction(pp.replaceWith("Feb")) |
+    pp.Literal("02").setParseAction(pp.replaceWith("Feb")) |
+    pp.Literal("3").setParseAction(pp.replaceWith("Mar")) |
+    pp.Literal("03").setParseAction(pp.replaceWith("Mar")) |
+    pp.Literal("4").setParseAction(pp.replaceWith("Apr")) |
+    pp.Literal("04").setParseAction(pp.replaceWith("Apr")) |
+    pp.Literal("5").setParseAction(pp.replaceWith("May")) |
+    pp.Literal("05").setParseAction(pp.replaceWith("May")) |
+    pp.Literal("6").setParseAction(pp.replaceWith("Jun")) |
+    pp.Literal("06").setParseAction(pp.replaceWith("Jun")) |
+    pp.Literal("7").setParseAction(pp.replaceWith("Jul")) |
+    pp.Literal("07").setParseAction(pp.replaceWith("Jul")) |
+    pp.Literal("8").setParseAction(pp.replaceWith("Aug")) |
+    pp.Literal("08").setParseAction(pp.replaceWith("Aug")) |
+    pp.Literal("9").setParseAction(pp.replaceWith("Sep")) |
+    pp.Literal("09").setParseAction(pp.replaceWith("Sep")) |
+    pp.Literal("10").setParseAction(pp.replaceWith("Oct")) |
+    pp.Literal("11").setParseAction(pp.replaceWith("Nov")) |
+    pp.Literal("12").setParseAction(pp.replaceWith("Dec"))
+)
+date_year = pp.Word(pp.nums)
+date_my = pp.Combine(date_month + pp.Literal("-").setParseAction(pp.replaceWith(" ")) + date_year)
 date_none = pp.Literal("-").setParseAction(pp.replaceWith("Current"))
-date = date_format | date_none
+date = date_my | date_none
 
 parens = lambda inner: pp.Dict(pp.Group(lp + inner + rp))
 
@@ -37,12 +61,12 @@ role = expr_quoted("role")
 
 expr_list = lambda key: expr(key, pp.OneOrMore(quoted_string))
 bullets = expr_list("bullets")
-items = expr_list("items")
+data = expr_list("data")
 
 
 # Resume section grammar
 entry = lambda params_list: lp + pp.Suppress("entry") + pp.Dict(pp.Each(params_list)) + rp
-expr_list_of_entry = lambda name, params_list: expr(name, pp.OneOrMore(pp.Group(entry(params_list))))
+expr_list_of_entry = lambda name, params_list: expr(name, pp.Group(pp.OneOrMore(pp.Group(entry(params_list)))))
 
 info_links = expr("links", pp.OneOrMore(linkedin | github | portfolio))
 info = expr("info", pp.Each((name, email, phone, location, info_links)))
@@ -50,7 +74,7 @@ info = expr("info", pp.Each((name, email, phone, location, info_links)))
 education_gpa = expr("gpa", pp.Word(pp.nums + "." + pp.nums))
 education = expr_list_of_entry("education", (name, degree, location, date_start, date_end, education_gpa))
 
-skills = expr_list_of_entry("skills", (category, items))
+skills = expr_list_of_entry("skills", (category, data))
 
 work = expr_list_of_entry("work", (company, role, location, date_start, date_end, bullets))
 
